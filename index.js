537e563bb10f9c472d25482da70f2c803b81c81b
Moises-Emiliano-Hernandez-Contreras <73419469+Moises-Emiliano-Hernandez-Contreras@users.noreply.github.com>
2022-09-20 09:51:05 -0500

    funcion tricolor

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ document.body.appendChild(canvas2);
 var grises = document.getElementById("grises");
 var limp = document.getElementById("limpiar");
 var inv = document.getElementById("inv");
+var sep = document.getElementById("sepia");
+var tri = document.getElementById("tri");
 function previewFile() {
     var img = new Image();
     var file = document.querySelector('input[type=file]');
@@ -23,17 +25,41 @@ function previewFile() {
         img.src = reader.result;
         img.onload = function () {
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-            grises.addEventListener("click", function gray() {
-                gris(img);
-            });
             limp.addEventListener("click", limpiar);
+            grises.addEventListener("click", function () { gris(img); });
             inv.addEventListener("click", function () { invertir(img); });
+            sep.addEventListener("click", function () { sepia(img); });
+            tri.addEventListener("click", function () { triColor(img); });
         };
     }, false);
     if (fi) {
         reader.readAsDataURL(fi);
     }
 }
+var triColor = function (img) {
+    ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+    var imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height);
+    var pixels = imageData.data;
+    var numPixels = imageData.width * imageData.height;
+    numPixels = Math.ceil(numPixels / 3);
+    for (var i = 0; i < numPixels; i++) {
+        pixels[i * 4] = 0;
+        pixels[i * 4 + 1] = pixels[i * 4 + 1];
+        pixels[i * 4 + 2] = 0;
+    }
+    for (var j = numPixels; j < numPixels * 2; j++) {
+        var grey = (pixels[j * 4] + pixels[j * 4 + 1] + pixels[j * 4 + 2]) / 3;
+        pixels[j * 4] = grey;
+        pixels[j * 4 + 1] = grey;
+        pixels[j * 4 + 2] = grey;
+    }
+    for (var k = numPixels * 2; k < numPixels * 3; k++) {
+        pixels[k * 4] = pixels[k * 4];
+        pixels[k * 4 + 1] = 0;
+        pixels[k * 4 + 2] = 0;
+    }
+    ctx2.putImageData(imageData, 0, 0);
+};
 var gris = function (img) {
     ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height);
     var imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height);
@@ -58,6 +84,22 @@ var invertir = function (img) {
         pixels[i * 4] = 255 - r;
         pixels[i * 4 + 1] = 255 - g;
         pixels[i * 4 + 2] = 255 - b;
+    }
+    ctx2.putImageData(imageData, 0, 0);
+};
+var sepia = function (img) {
+    ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+    var imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height);
+    var pixels = imageData.data;
+    var numPixels = imageData.width * imageData.height;
+    for (var i = 0; i < numPixels; i++) {
+        var r = pixels[i * 4], g = pixels[i * 4 + 1], b = pixels[i * 4 + 2];
+        pixels[i * 4] = 255 - r;
+        pixels[i * 4 + 1] = 255 - g;
+        pixels[i * 4 + 2] = 255 - b;
+        pixels[i * 4] = (r * .393) + (g * .769) + (b * .189);
+        pixels[i * 4 + 1] = (r * .349) + (g * .686) + (b * .168);
+        pixels[i * 4 + 2] = (r * .272) + (g * .534) + (b * .131);
     }
     ctx2.putImageData(imageData, 0, 0);
 };
--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+
 let canvas = <HTMLCanvasElement>document.createElement('canvas');
 let ctx = canvas.getContext("2d")!;
 canvas.width = 400;
@@ -16,6 +17,8 @@ document.body.appendChild(canvas2);
 let grises = document.getElementById("grises")! as HTMLButtonElement;
 let limp = document.getElementById("limpiar")! as HTMLButtonElement;
 let inv= document.getElementById("inv")! as HTMLButtonElement;
+let sep = document.getElementById("sepia")! as HTMLButtonElement;
+let tri = document.getElementById("tri")! as HTMLButtonElement;
 
 function previewFile() {
       let img = new Image()
@@ -26,12 +29,11 @@ function previewFile() {
             img.src = reader.result as string;
             img.onload = () => {
                   ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-                  grises.addEventListener("click", function gray() {
-                        gris(img)
-                  })
                   limp.addEventListener("click", limpiar)
-                  inv.addEventListener("click",function (){invertir(img)})
-
+                  grises.addEventListener("click", function (){gris(img)})
+                  inv.addEventListener("click",function (){invertir(img)})                  
+                  sep.addEventListener("click",function(){sepia(img)})
+                  tri.addEventListener("click",function(){triColor(img)})
             }
       }, false);
 
@@ -39,6 +41,34 @@ function previewFile() {
             reader.readAsDataURL(fi);
       }
 }
+const triColor=(img:any)=>{
+      ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height)
+      var imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height);
+      var pixels = imageData.data
+      var numPixels = imageData.width * imageData.height;
+      numPixels=Math.ceil(numPixels/3)      
+      for (var i = 0; i < numPixels; i++){  
+            pixels[i * 4] = 0;
+            pixels[i * 4 + 1] = pixels[i * 4 + 1];
+            pixels[i * 4 + 2] = 0;
+      }  
+      for (var j = numPixels; j < numPixels*2; j++){
+            var grey=(pixels[j * 4]+pixels[j * 4 + 1]+pixels[j * 4 + 2])/3
+            pixels[j * 4] = grey;
+            pixels[j * 4 + 1] = grey;
+            pixels[j * 4 + 2] = grey;
+      }
+      for (var k = numPixels*2; k < numPixels*3; k++){
+                  pixels[k * 4] = pixels[k * 4];
+                  pixels[k * 4 + 1] = 0;
+                  pixels[k * 4 + 2] = 0;
+      }          
+      ctx2.putImageData(imageData, 0, 0);
+}
+
+
+
+
 const gris = (img: any) => {
       ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height)
       var imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height);
@@ -69,6 +99,26 @@ const invertir = (img:any) => {
             pixels[ i * 4 + 2 ] = 255 - b;            
       }
       ctx2.putImageData(imageData, 0, 0);
+}
+const sepia=(img:any)=>{
+      ctx2.drawImage(img, 0, 0, canvas2.width, canvas2.height);
+      var imageData = ctx.getImageData(0, 0, canvas2.width, canvas2.height);
+      var pixels = imageData.data
+      var numPixels = imageData.width * imageData.height;
+
+      for(var i=0;i<numPixels;i++){
+            var r = pixels[i * 4],
+            g = pixels[i * 4 + 1],
+            b = pixels[i * 4 + 2];
+            pixels[ i * 4 ] = 255 - r;
+            pixels[ i * 4 + 1 ] = 255 - g;
+            pixels[ i * 4 + 2 ] = 255 - b;
+     
+            pixels[ i * 4 ] = ( r * .393 ) + ( g *.769 ) + ( b * .189 );
+            pixels[ i * 4 + 1 ] = ( r * .349 ) + ( g *.686 ) + ( b * .168 );
+            pixels[ i * 4 + 2 ] = ( r * .272 ) + ( g *.534 ) + ( b * .131 );           
+      }
+      ctx2.putImageData(imageData, 0, 0);      
 }
 const limpiar = () => {
       ctx2.clearRect(0, 0, canvas2.width, canvas2.height)
